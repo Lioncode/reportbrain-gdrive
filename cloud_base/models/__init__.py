a11ca0acae6e87fa4047df8251338f1d8fd8c6b0
Ioannis Papadopoulos <support@lioncode.gr>
2020-09-22 16:16:25 +0300

    first commit

--- cloud_base/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import ir_attachment
-from . import cloud_domain_period
-from . import sync_model
-from . import sync_object
-from . import res_config_settings
-from . import sync_log
